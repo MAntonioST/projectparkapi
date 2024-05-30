7df8041d6e74985cd669bfa6112d9514f5e45e7e
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2024-05-30 12:17:10 -0300

    Project started

---
+++ src/main/java/com/marcot/projectparkapi/ProjectParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcot.projectparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectParkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectParkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcot/projectparkapi/ProjectParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcot.projectparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
