875454a208a6507626d4b37198dd382ff517965c
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2024-05-31 09:19:06 -0300

    feat: Add save user functionality

--- src/main/java/com/marcot/projectparkapi/controller/UserController.java
+++ src/main/java/com/marcot/projectparkapi/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.marcot.projectparkapi.controller;
 
 
+import com.marcot.projectparkapi.entity.User;
 import com.marcot.projectparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User entity){
+        User user = userService.createUser(entity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
--- src/main/java/com/marcot/projectparkapi/service/UserService.java
+++ src/main/java/com/marcot/projectparkapi/service/UserService.java
@@ -1,13 +1,21 @@
 package com.marcot.projectparkapi.service;
 
 
+import com.marcot.projectparkapi.entity.User;
 import com.marcot.projectparkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User createUser(User entity) {
+        userRepository.save(entity);
+        return entity;
+    }
 }
