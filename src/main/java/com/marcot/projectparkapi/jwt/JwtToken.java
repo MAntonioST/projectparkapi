f789e2debe8986c4bf2db4645111a8a7f20907e3
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2024-06-23 09:34:22 -0300

    feat: add Json Web Token

---
+++ src/main/java/com/marcot/projectparkapi/jwt/JwtToken.java
@@ -0,0 +1,14 @@
+package com.marcot.projectparkapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class JwtToken {
+    private String token;
+}
